4515b7c0b74b66085ff047489b4380811ff9a74e
martastaporek <staporekmarta1@gmail.com>
2018-07-06 13:29:55 +0200

    Implement basic func of get and post

--- src/main/java/SimpleHttpServlet.java
+++ src/main/java/SimpleHttpServlet.java
@@ -4,13 +4,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 public class SimpleHttpServlet extends HttpServlet {
 
-    protected void doGet( HttpServletRequest request,
-                          HttpServletResponse response)
-            throws IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 
-        response.getWriter().write(HTMLGenerator.generateHTML());
+        try {
+
+            response.getWriter().write(HTMLGenerator.generateHTML());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+        try {
+            String text = request.getParameter("text");
+            String upperText = text.toUpperCase();
+            response.getWriter().write(upperText);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
